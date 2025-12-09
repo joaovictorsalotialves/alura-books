365e09218a908ea5e8acd70166a000c085491040
João Victor <joaovictorsalotialves12@gmail.com>
2025-12-09 15:36:15 -0300

    Criando containers

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,21 @@
-import './styles.css'
+import styled from "styled-components";
 
 import Logo from '../Logo'
 import OptionsHeader from '../OptionsHeader'
 import IconsHeader from '../IconsHeader'
 
+const HeaderContainer = styled.header`
+  background-color: #FFF;
+  display: flex;
+  justify-content: center;
+`
+
 export default function Header() {
   return (
-    <header className='App-header'>
+    <HeaderContainer>
       <Logo />
       <OptionsHeader />
       <IconsHeader />
-    </header>
+    </HeaderContainer>
   )
 }
